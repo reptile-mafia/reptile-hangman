62e3b8238c044371740d300851afe338f97bf098
MKS-students <mks.students@hackreactor.com>
2016-08-26 10:30:50 -0500

    Sets up server test framework

---
+++ server/tests/ServerSpec.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+
+describe('Does this work', function() {
+  it('should fail a test', function() {
+    expect(true).to.equal(false);
+  });
+});
