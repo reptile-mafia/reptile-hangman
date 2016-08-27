b9cddf070eb5086ac03c34857f5859e11a165467
siliconion <chihchiayang@gmail.com>
2016-08-26 19:12:12 -0500

    refactor test folder structure

---
+++ test/client/client.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {expect} from 'chai';
+import {shallow, mount, render} from 'enzyme';
+import GameBoard from '../../client/components/GameBoard';
+
+describe('Guessed Letters', function() {
+
+	it('Should show correct letters', ()=>{
+		//let app = render(<GameBoard />);
+		//expect(app.find('.guessedLetters').children.length.to.equal(0);
+		expect(true).to.equal(true);
+	})
+
+	// it('Should reset to empty at game initialization', () => {
+	// 	let app = render(<GameBoard />);
+	// 	expect(app.find('.guessedLetters').children.length.to.equal(0);
+
+	// })
+});
