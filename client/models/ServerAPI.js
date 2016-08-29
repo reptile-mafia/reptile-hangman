ce6b68b5da43e44b03b2bc0741532dbc7ef56306
Christopher Kiel <no-fi@users.noreply.github.com>
2016-08-29 12:33:03 -0500

    Initial outline of ServerAPI

---
+++ client/models/ServerAPI.js
@@ -0,0 +1,33 @@
+import io from 'socket.io-client';
+
+export default class ServerAPI {
+
+  constructor() {
+
+  }
+
+  onStartGame(callback) {
+
+  }
+
+  makeGuess() {
+
+  }
+
+  onIncorrectGuess(callback) {
+
+  }
+
+  onCorrectGuess(callback) {
+
+  }
+
+  onWin(callback) {
+
+  }
+
+  onLose(callback) {
+
+  }
+
+}
--- test/server/serversocket.spec.js
+++ test/server/serversocket.spec.js
@@ -1,6 +1,5 @@
 var chai = require('chai');
 var expect = chai.expect;
-var app = require('../../server/main.js');
 var io = require('socket.io-client');
 var server = require('http').createServer();
 var server_io = require('socket.io')(server);
