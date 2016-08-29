e7ab13e2eba9be489e20a77963c317d4def5c4f1
siliconion <chihchiayang@gmail.com>
2016-08-29 11:55:08 -0500

    add alphabet on click function, a little bit css

--- client/components/Alphabets.js
+++ client/components/Alphabets.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Models from '../models/ServerAPI'
 
 export default class Alphabets extends React.Component {
 
@@ -7,17 +8,19 @@ export default class Alphabets extends React.Component {
 		this.alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 	}
 
-	onAlphabetClick(e, target){
-		target
+	onAlphabetClick(e){
+		//target
+		console.log(e.target);
+		//Models.guesses(e.target.text())
 	}
 
 	render() {
 		return ( 
 			< div className = "alphabets"> 
 				{
-					this.alphabets.map(function(alphabet) {
+					this.alphabets.map((alphabet) => {
 						return ( 
-							< span className = "alphabet" key = {alphabet} onClick = {(e)=>{onAlphabetClick(e, this)}}> 
+							< span className = "alphabet" key = {alphabet} onClick = {(e)=>{this.onAlphabetClick(e)}}> 
 								{ alphabet } 
 							< /span> )
 					})
