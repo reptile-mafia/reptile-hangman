e2e31bb10a856e63308b4f90348f6cb93bee06f0
Doug Riblet <smashzen85@gmail.com>
2016-08-29 16:02:16 -0500

    clear text from Outcome div

--- client/components/Outcome.js
+++ client/components/Outcome.js
@@ -8,7 +8,7 @@ export default class Outcome extends React.Component {
 
 	render() {
 		return ( 
-			< div id="displayOutcome"> You Win! or You Lose!< /div>
+			< div id="displayOutcome">< /div>
 		)
 	}
 }
