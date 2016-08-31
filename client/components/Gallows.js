b4b87cd116ed70e7996b9d2c5934c14eaa02156a
Doug Riblet <smashzen85@gmail.com>
2016-08-31 13:11:29 -0500

    remainingGuess sets body parts opacity

--- client/components/Gallows.js
+++ client/components/Gallows.js
@@ -22,12 +22,24 @@ export default class Gallows extends React.Component {
 						<circle cx="275" cy="180" r="10" />
 					</g>
 					<g id="gallowMan">
-						<circle className="noggin miss1" cx="275" cy="158" r="30" opacity="1" />
-						<line className="torso miss2" x1="275" y1="190" x2="275" y2="290" opacity="1" />
-						<line className="arm-left miss3" x1="275" y1="220" x2="220" y2="250" opacity="1" />
-						<line className="arm-right miss4" x1="275" y1="220" x2="330" y2="250" opacity="1" />
-						<line className="leg-left miss5" x1="275" y1="285" x2="230" y2="350" opacity="1" />
-						<line className="leg-right miss6" x1="275" y1="285" x2="320" y2="350" opacity="1" />
+						<circle id="noggin" 
+							className={this.props.remainingGuess < 6 ? "op-on" : "op-off"}
+							cx="275" cy="158" r="30" />
+						<line id="torso" 
+							className={this.props.remainingGuess < 5 ? "op-on" : "op-off"}
+							x1="275" y1="190" x2="275" y2="290" />
+						<line id="arm-left" 
+							className={this.props.remainingGuess < 4 ? "op-on" : "op-off"}
+							x1="275" y1="220" x2="220" y2="250" />
+						<line id="arm-right" 
+							className={this.props.remainingGuess < 3 ? "op-on" : "op-off"}
+							x1="275" y1="220" x2="330" y2="250" />
+						<line id="leg-left" 
+							className={this.props.remainingGuess < 2 ? "op-on" : "op-off"}
+							x1="275" y1="285" x2="230" y2="350" />
+						<line id="leg-right" 
+							className={this.props.remainingGuess < 1 ? "op-on" : "op-off"}
+							x1="275" y1="285" x2="320" y2="350" />
 					</g>
 				</svg>
 			</div>
