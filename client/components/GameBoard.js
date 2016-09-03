4b32a5e9a89b43913abef67d0cd66c7826c871fb
Doug Riblet <smashzen85@gmail.com>
2016-09-03 14:02:27 -0500

    bootstrap basic layout, add css link

--- client/components/GameBoard.js
+++ client/components/GameBoard.js
@@ -13,16 +13,37 @@ export default class GameBoard extends React.Component {
 	}
 
 	render() {
-		var guessedLettersUpper = this.props.guessedLetters.map((letter)=>{return letter.toUpperCase()});
-		return(
-			<div className ='gameBoard'>	
-				<Gallows remainingGuesses={this.props.remainingGuesses} />
-				<RemainingGuess remainingGuesses={this.props.remainingGuesses} />
-				<Word word={this.props.word} />
-				<GuessedLetters guessedLetters={guessedLettersUpper} />
-				<Alphabets guessedLetters={guessedLettersUpper} models = {this.props.models}/>	
+		// var guessedLettersUpper = this.props.guessedLetters.map((letter)=>{return letter.toUpperCase()});
+		// return(
+		// 	<div className ='gameBoard'>	
+		// 		<Gallows remainingGuesses={this.props.remainingGuesses} />
+		// 		<RemainingGuess remainingGuesses={this.props.remainingGuesses} />
+		// 		<Word word={this.props.word} />
+		// 		<GuessedLetters guessedLetters={guessedLettersUpper} />
+		// 		<Alphabets guessedLetters={guessedLettersUpper} models = {this.props.models}/>	
+		// 	</div>
+		// )
+			<div className="container">
+				<div className="row">
+					<div className="col-xs-8" id="gallows">
+						<Gallows remainingGuess={this.state.remainingGuess} />
+					</div>
+					<div className="col-xs-4" id ="guessed">
+						<RemainingGuess remainingGuess={this.state.remainingGuess} />
+						<GuessedLetters guessedLetters={this.state.guessedLetters} />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-12" id="theword">
+						<Word word={this.state.word} />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-12" id="alphabet">
+						<Alphabet guessedLetters={this.state.guessedLetters} />
+					</div>
+				</div>
 			</div>
-		)
 	}
 
 }
