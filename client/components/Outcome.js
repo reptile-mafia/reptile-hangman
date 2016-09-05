41dd0267145c79d23075d8826eb41dc3480e390f
Doug Riblet <smashzen85@gmail.com>
2016-09-05 10:20:39 -0500

    Modal using react-bootstrap

--- client/components/Outcome.js
+++ client/components/Outcome.js
@@ -1,26 +1,44 @@
+ 
 import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
 
 export default class Outcome extends React.Component {
 
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {    
+      showModal: false
+    };
+  }
 
 	onPlayAgain(){
 		this.props.models.playAgain();
 	}
 
-	render() {
-		return ( 
-			< div className="outcome-outline modal">
-				<div className="outcome">
-					{this.props.outcome.win?"WIN":"LOSE"}
-					<br/>
-					{this.props.outcome.player.slice(10)}
-					<div onClick = {this.onPlayAgain.bind(this)}>Play Again?</div>
-				</div>
-			< /div>
+  close() {
+    this.setState({ showModal: false });
+  }
 
-		)
-	}
+  open() {
+    this.setState({ showModal: true });
+  }
+
+  render() {
+    return ( 
+      <div>
+      <Modal show={this.state.showModal} onHide={this.close}>
+        <Modal.Header closeButton>
+          <Modal.Title>{this.props.gameState}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>The word was <strong>#######</strong></p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.close}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+      </div>
+    )
+  }
 }
+
