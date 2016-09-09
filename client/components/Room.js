41567575ddfa79ec5be6d38c7d176b99b946c6bf
Ryan Walter <r.walter215@gmail.com>
2016-09-09 11:39:51 -0500

    partial win recording functionality

--- client/components/Room.js
+++ client/components/Room.js
@@ -117,6 +117,14 @@ export default class Room extends React.Component {
     });
   }
 
+  recordWin() {
+    var winCountRef = firebase.database().ref('users/cH74Wy1ASje2tx7TKrnUFvJNrxe2/winCount');
+    winCountRef.transaction((currentRank) => {
+      // If users/ada/rank has never been set, currentRank will be `null`.
+      return currentRank + 1;
+    });
+  }
+
   selectGameMode() {
     const guessedLettersUpper = this.state.guessedLetters !== null ? this.state.guessedLetters.map(letter => letter.toUpperCase()) : [];
 
